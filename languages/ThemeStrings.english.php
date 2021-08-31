56661573f1d1fc137ada8d0fbd7b6c8d21b6c315
CeeMoo <qdale.123@gmail.com>
2021-09-01 01:47:45 +0300

    Initial commit

---
+++ languages/ThemeStrings.english.php
@@ -0,0 +1,26 @@
+<?php
+
+$txt['themecopyright'] = ' <strong>ServRamadana Theme By <a href="https://smf.konusal.com/" target="_blank">Cee</a></strong>';
+
+
+$txt['bese_color_default'] = 'bese_color_default';
+$txt['bese_color_primary'] = 'bese_color_primary';
+$txt['bese_color_secondary'] = 'bese_color_secondary';
+$txt['bese_default_mode'] = 'bese_default_mode';
+
+$txt['bese_color_defaulte'] = 'Default is: %color%';
+
+
+$txt['bese_default_mode'] = 'Default Mode';
+$txt['bese_dark_mode'] = 'Dark Mode';
+$txt['bese_light_mode'] = 'Light Mode';
+$txt['bese_theme_settings'] = 'ServRamadana Theme Settings';
+
+$txt['bese_allow_user_modes'] = 'bese_allow_user_modes';
+$txt['bese_allow_color_select'] = 'bese_allow_color_select';
+
+$txt['default_bese_color'] = '#ebe0c0';
+$txt['default_bese_primary'] = '#be9a3b';
+$txt['default_bese_secondary'] = '#a89e7a';
+
+?>
